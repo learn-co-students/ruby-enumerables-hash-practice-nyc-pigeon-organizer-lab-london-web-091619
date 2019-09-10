39dae3df25fb5fe43dac7953e86c54965fee92cf
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-10 09:45:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+#def nyc_pigeon_organizer(data)
+ # data.each_with_object({}) do (key, h), result| ??
+ #   h.each do {value, names}  ?????
+ #     names.each do (name) ??? 
+ 
+ ###WHY is this not working??? 
+ ##AARRRGGGHH!!!!
+ 
+ 
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
 end
+
