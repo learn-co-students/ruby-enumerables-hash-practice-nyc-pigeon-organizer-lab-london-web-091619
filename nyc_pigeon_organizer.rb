5b24164091c24d3975f5ef64ab4b61edf3b6b2e7
Aniket B <aniket.b@outlook.com>
2019-09-12 12:20:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |level, sublevel|
+    sublevel.each do |details, names|
+      names.each do |name|
+        if list.key(name) == nil
+          list["#{name}"] = {:color => [], :gender => [], :lives => []}
+        end
+      end
+    end
+  end
+  data.each do |level, sublevel|
+    sublevel.each do |details, names|
+      names.each do |name|
+        list["#{name}"][level] << details.to_s
+      end
+    end
+  end
+  list
 end
