da0112b4a911c036e43f8356e394a18a1bddc5fc
Luke Prendergast <luke.prendergast94@gmail.com>
2019-09-12 11:54:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each { |color_gender_lives, attributes|
+    attributes.each { |attribute_value, pigeon_names|
+      pigeon_names.each { |individual_name|
+        pigeon_list[individual_name] ||= {}
+        pigeon_list[individual_name][color_gender_lives] ||= []
+        pigeon_list[individual_name][color_gender_lives].push(attribute_value.to_s)
+      }
+    } 
+  }
+  pigeon_list
 end
